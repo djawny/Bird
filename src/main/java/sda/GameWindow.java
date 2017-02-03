409fe0a0f5b21fc8f8a24f9948a7c8ee17543f63
djawny <djawny@op.pl>
2017-02-03 18:33:57 +0100

    zmiana 3/02

--- src/main/java/sda/GameWindow.java
+++ src/main/java/sda/GameWindow.java
@@ -12,14 +12,18 @@ public class GameWindow extends JFrame implements KeyListener {
         setTitle("Flappy Angry Bird");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLayout(new FlowLayout());
-        gamePanel = new GamePanel();
-        gamePanel.setPreferredSize(gamePanel.getSIZE());
-        gamePanel.setBackground(Color.cyan);
-        add(gamePanel);
+        createGamePanel();
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
         addKeyListener(this);
+    }
+
+    private void createGamePanel() {
+        gamePanel = new GamePanel();
+        gamePanel.setPreferredSize(gamePanel.getSIZE());
+        gamePanel.setBackground(Color.cyan);
+        add(gamePanel);
     }
 
     @Override
@@ -29,26 +33,42 @@ public class GameWindow extends JFrame implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-            gamePanel.setVerticalDirection(-1);
-            gamePanel.getBird().setYLimit(gamePanel.getBird().getY() - 40);
-            gamePanel.getBird().setStep(25);
-        }
-        if (e.getKeyCode() == KeyEvent.VK_ESCAPE && gamePanel.isGameOver()) {
-            gamePanel.restart();
-        }
-        if (e.getKeyCode() == KeyEvent.VK_EQUALS || e.getKeyCode() == KeyEvent.VK_ADD) {
-            gamePanel.getGameTimer().setDelay(gamePanel.getGameTimer().getDelay() + 1);
-        }
+        birdJump(e);
+        restartGame(e);
+        addDelay(e);
+        subtractDelay(e);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    private void subtractDelay(KeyEvent e) {
         if ((e.getKeyCode() == KeyEvent.VK_MINUS || e.getKeyCode() == KeyEvent.VK_SUBTRACT)
                 && gamePanel.getGameTimer().getDelay() > 0) {
             gamePanel.getGameTimer().setDelay(gamePanel.getGameTimer().getDelay() - 1);
         }
     }
 
-    @Override
-    public void keyReleased(KeyEvent e) {
+    private void addDelay(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_EQUALS || e.getKeyCode() == KeyEvent.VK_ADD) {
+            gamePanel.getGameTimer().setDelay(gamePanel.getGameTimer().getDelay() + 1);
+        }
+    }
 
+    private void restartGame(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE && gamePanel.isGameOver()) {
+            gamePanel.restart();
+        }
+    }
+
+    private void birdJump(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            gamePanel.setVerticalDirection(-1);
+            gamePanel.getBird().setYLimit(gamePanel.getBird().getY() - 40);
+            gamePanel.getBird().setStep(25);
+        }
     }
 }
 
